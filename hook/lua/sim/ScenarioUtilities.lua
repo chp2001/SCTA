d659f744d7671121a5ef2b1eaae3d8f17d35e75a
Dragun <poyosh2@aol.com>
2020-06-13 12:23:51 -0400

    ai fix please

--- hook/lua/sim/ScenarioUtilities.lua
+++ hook/lua/sim/ScenarioUtilities.lua
@@ -1,21 +1,28 @@
-do
-    local OldCreateInitialArmyGroup = CreateInitialArmyGroup
-    function CreateInitialArmyGroup(strArmy, createCommander)
-        if createCommander then
-            local AIBrain = GetArmyBrain(strArmy)
-        end
-        return OldCreateInitialArmyGroup(strArmy, createCommander)
-    end
-
     local OldCreateResources = CreateResources
+    function CreateInitialArmyGroup(strArmy, createCommander)
+        local tblGroup = CreateArmyGroup( strArmy, 'INITIAL')
+        local cdrUnit = false
+    
+        if createCommander and ( tblGroup == nil or 0 == table.getn(tblGroup) ) then
+            local factionIndex = GetArmyBrain(strArmy):GetFactionIndex()
+            local initialUnitName = import('/lua/factions.lua').Factions[factionIndex].InitialUnit
+            cdrUnit = CreateInitialArmyUnit(strArmy, initialUnitName)
+            if EntityCategoryContains(categories.COMMAND, cdrUnit) then
+                if ScenarioInfo.Options['PrebuiltUnits'] == 'Off' then
+                    #cdrUnit:HideBone(0, true)
+                    ForkThread(CommanderWarpDelay, cdrUnit, 3)
+                end
+            end
+        end
+    
+
+
     function CreateResources()
         if not ScenarioInfo.WindStats then
             ScenarioInfo.WindStats = {Thread = ForkThread(WindThread)}
         end
         OldCreateResources()
     end
-
-
 
     function WindThread()
         WaitTicks(26)
@@ -25,7 +32,7 @@ do
         local max = math.max
         local mod = math.mod
         while true do
-            ScenarioInfo.WindStats.Power = min(max( (ScenarioInfo.WindStats.Power or 0.5) + 0.5 - random(),0),1)
+            ScenarioInfo.WindStats.Power = min(max((ScenarioInfo.WindStats.Power or 0.5) + 0.5 - random(),0),1)
             --Defines a real number, starting from 0.5, between 0 and 1 that randomly fluctuates by up to 0.5 either direction.
             --math.random() with no args returns a real number between 0 and 1
             ScenarioInfo.WindStats.Direction = mod((ScenarioInfo.WindStats.Direction or random(0,360)) + random(-5,5) + random(-5,5) + random(-5,5) + random(-5,5), 360)

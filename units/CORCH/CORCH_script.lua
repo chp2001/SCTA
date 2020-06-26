0d3371dcfb5687d0d05270abfb25a846b75c748b
Dragun <poyosh2@aol.com>
2020-06-25 21:14:45 -0400

    minor update

--- units/CORCH/CORCH_script.lua
+++ units/CORCH/CORCH_script.lua
@@ -10,8 +10,6 @@ CORCH = Class(TAconstructor) {
 
 	OnCreate = function(self)
 		self.Spinners = {
-			door1 = CreateRotator(self, 'door1', 'z', nil, 0, 0, 0),
-			door2 = CreateRotator(self, 'door2', 'z', nil, 0, 0, 0),
 			nanogun = CreateRotator(self, 'nanogun', 'x', nil, 0, 0, 0),
 			turret = CreateRotator(self, 'turret', 'y', nil, 0, 0, 0),
 			post = CreateRotator(self, 'post', 'x', nil, 0, 0, 0),
@@ -29,49 +27,6 @@ CORCH = Class(TAconstructor) {
 			self.Trash:Add(v)
 		end
 		TAconstructor.OnCreate(self)
-	end,
-
-	Open = function(self)
-
-		--TURN door1 to z-axis <-67.14> SPEED <99.20>;
-		self.Spinners.door1:SetGoal(67.14)
-		self.Spinners.door1:SetSpeed(99.20)
-
-		--TURN door2 to z-axis <67.14> SPEED <99.20>;
-		self.Spinners.door2:SetGoal(-67.14)
-		self.Spinners.door2:SetSpeed(99.20)
-
-		--SLEEP <677>;
-		WaitSeconds(0.7)
-
-		--MOVE door1 to y-axis <-2.00> SPEED <2.00>;
-		self.Sliders.door1:SetGoal(-2,-0.75,0)
-		self.Sliders.door1:SetSpeed(2)
-
-		--MOVE door2 to y-axis <-2.00> SPEED <2.00>;
-		self.Sliders.door2:SetGoal(2,-0.75,0)
-		self.Sliders.door2:SetSpeed(2)
-
-		--MOVE plate to y-axis <3.99> SPEED <5.00>;
-		self.Sliders.plate:SetGoal(0,3.5,0) #too high
-		self.Sliders.plate:SetSpeed(5)
-
-		--SLEEP <688>;
-		WaitSeconds(0.7)
-
-		--TURN post to x-axis <90.00> SPEED <131.03>;
-		self.Spinners.post:SetGoal(90)
-		self.Spinners.post:SetSpeed(131.03)
-
-		--TURN nanogun to x-axis <-90.00> SPEED <160.03>;
-		self.Spinners.nanogun:SetGoal(-90)
-		self.Spinners.nanogun:SetSpeed(160.03)
-
-		--SLEEP <687>;
-		WaitSeconds(0.7)
-
-		--SLEEP <39>;
-		TAconstructor.Open(self)
 	end,
 
 	Aim = function(self,target)
@@ -122,20 +77,6 @@ CORCH = Class(TAconstructor) {
 		--MOVE plate to y-axis <0> SPEED <8.00>;
 		self.Sliders.plate:SetGoal(0,0,0)
 		self.Sliders.plate:SetSpeed(8)
-
-		--SLEEP <471>;
-		WaitSeconds(0.45)
-
-		--TURN door1 to z-axis <0> SPEED <143.20>;
-		self.Spinners.door1:SetGoal(0)
-		self.Spinners.door1:SetSpeed(143.20)
-
-		--TURN door2 to z-axis <0> SPEED <143.20>;
-		self.Spinners.door2:SetGoal(0)
-		self.Spinners.door2:SetSpeed(143.20)
-
-		--SLEEP <469>;
-		WaitSeconds(0.45)
 
 		--SLEEP <27>;
 		TAconstructor.Close(self)

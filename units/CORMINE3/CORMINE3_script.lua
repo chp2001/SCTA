4f96d763dd6707c8080b1c288fc9a27c3fdbfa75
Dragun <poyosh2@aol.com>
2020-07-30 15:13:11 -0400

    threats assigned

---
+++ units/CORMINE3/CORMINE3_script.lua
@@ -0,0 +1,43 @@
+#CORE Focused Mine - Med. Damage, Small Range Mine
+#CORMINE3
+#
+#Script created by Raevn
+
+local TAunit = import('/mods/SCTA-master/lua/TAunit.lua').TAunit
+local Projectile = import('/lua/sim/DefaultWeapons.lua').DefaultProjectileWeapon
+
+CORMINE3 = Class(TAunit) {
+	attacked = false,
+
+	Weapons = {
+		CORMINE3 = Class(Projectile) {
+			OnWeaponFired = function(self)
+				self.unit.attacked = true
+				self.unit:Kill()
+			end,
+		},
+	},
+
+	OnKilled = function(self, instigator, type, overkillRatio)
+		if self.unit.attacked == true then
+			instigator = self
+		end
+		TAunit.OnKilled(self, instigator, type, overkillRatio)
+		
+	end,
+
+	OnStopBeingBuilt = function(self,builder,layer)
+		TAunit.OnStopBeingBuilt(self,builder,layer)
+		self:SetScriptBit('RULEUTC_CloakToggle', true)
+		self:RequestRefreshUI()
+	end,
+
+	OnIntelDisabled = function(self)
+		self:SetMesh(self:GetBlueprint().Display.MeshBlueprint, true)
+	end,
+
+	OnIntelEnabled = function(self)
+		self:SetMesh('/mods/SCTA-master/units/CORMINE3/CORMINE3_cloak_mesh', true)
+	end,
+}
+TypeClass = CORMINE3

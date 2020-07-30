6158fc7c02fad00915d616b5190b660a374fa1f6
Dragun <poyosh2@aol.com>
2020-07-29 20:25:09 -0400

    CoupleChanges, and Corbuzz

---
+++ projectiles/CORBUZZ_WEAPON/CORBUZZ_WEAPON_script.lua
@@ -0,0 +1,6 @@
+local TAHeavyCannonProjectile = import('/mods/SCTA-master/lua/TAProjectiles.lua').TAHeavyCannonProjectile
+
+CORBUZZ_WEAPON = Class(TAHeavyCannonProjectile) {
+}
+
+TypeClass = CORBUZZ_WEAPON
---
+++ units/CORBUZZ/CORBUZZ_Script.lua
@@ -0,0 +1,39 @@
+local TAunit = import('/mods/SCTA-master/lua/TAunit.lua').TAunit
+local TAweapon = import('/mods/SCTA-master/lua/TAweapon.lua').TAweapon
+
+CORBUZZ = Class(TAunit) {
+	currentBarrel = 0,
+
+	OnCreate = function(self)
+		TAunit.OnCreate(self)
+		self.Spinners = {
+			Spindle = CreateRotator(self, 'Spindle', 'x', nil, 0, 0, 0),
+		}
+		for k, v in self.Spinners do
+			self.Trash:Add(v)
+		end
+	end,
+
+	Weapons = {
+		CORBUZZ_WEAPON = Class(TAweapon) {
+			OnWeaponFired = function(self)
+				TAweapon.OnWeaponFired(self)
+				TAunit.ShowMuzzleFlare(self, 0.25)
+				self.unit.currentBarrel = self.unit.currentBarrel + 1
+				if self.unit.currentBarrel == 6 then
+					self.unit.currentBarrel = 0
+				end
+				self.unit:CreateProjectileAtBone('/mods/SCTA-master/effects/entities/Shells/ARMVULC_Shell/ARMVULC_Shell_proj.bp','Turret')
+			end,
+
+    			PlayFxRackReloadSequence = function(self)
+				--TURN spindle to z-axis <90> SPEED <400.09>; (for each turn)
+				self.unit.Spinners.Spindle:SetGoal(-90 * (self.unit.currentBarrel + 1))
+				self.unit.Spinners.Spindle:SetSpeed(480)
+
+				TAweapon.PlayFxRackReloadSequence(self)
+			end,
+		},
+	},
+}
+TypeClass = CORBUZZ

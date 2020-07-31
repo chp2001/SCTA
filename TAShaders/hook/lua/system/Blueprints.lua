dd7c28c5f959a3ba4907b078f0c5169558502f9b
Dragun <poyosh2@aol.com>
2020-07-31 13:32:49 -0400

    shaders

---
+++ TAShaders/hook/lua/system/Blueprints.lua
@@ -0,0 +1,27 @@
+local oldExtractBuildMeshBlueprint = ExtractBuildMeshBlueprint
+
+function ExtractBuildMeshBlueprint(bp)
+	oldExtractBuildMeshBlueprint(bp)
+	local FactionName = bp.General.FactionName
+	if FactionName == 'ARM' or FactionName == 'CORE' then 
+		local meshid = bp.Display.MeshBlueprint
+		if not meshid then return end
+
+		local meshbp = original_blueprints.Mesh[meshid]
+		if not meshbp then return end
+
+		local shadername = 'TABuild'
+		-- local secondaryname = '/textures/effects/' .. FactionName .. 'BuildSpecular.dds'
+
+		local buildmeshbp = table.deepcopy(meshbp)
+		if buildmeshbp.LODs then
+			for i,lod in buildmeshbp.LODs do
+				lod.ShaderName = shadername
+				-- lod.SecondaryName = secondaryname
+			end
+		end
+		buildmeshbp.BlueprintId = meshid .. '_build'
+		bp.Display.BuildMeshBlueprint = buildmeshbp.BlueprintId
+		MeshBlueprint(buildmeshbp)
+	end
+end

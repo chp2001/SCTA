4ac68837b52640bdce85eda6288a097b51995200
Dragun <poyosh2@aol.com>
2020-08-27 13:20:31 -0400

    CorViper Added

---
+++ units/CORVIPE/CORVIPE_Script.lua
@@ -0,0 +1,51 @@
+#ARM Ambusher - Pop-up Heavy Cannon
+#ARMAMB
+#
+#Script created by Raevn
+
+local TAunit = import('/mods/SCTA-master/lua/TAunit.lua').TAunit
+local TAweapon = import('/mods/SCTA-master/lua/TAweapon.lua').TAweapon
+
+CORVIPE = Class(TAunit) {
+	damageReduction = 1,
+
+	OnCreate = function(self)
+		TAunit.OnCreate(self)
+		self:SetWeaponEnabledByLabel('ARMAMB_GUN', false)
+	end,
+
+	OnStopBeingBuilt = function(self,builder,layer)
+		TAunit.OnStopBeingBuilt(self,builder,layer)
+		ForkThread(self.InitialPack,self)
+	end,
+
+	OnDamage = function(self, instigator, amount, vector, damageType)
+		TAunit.OnDamage(self, instigator, amount * self.damageReduction, vector, damageType)
+		#Has Damage Reduction
+	end,
+
+	InitialPack = function(self)
+		self.damageReduction = 0.28
+		self:SetWeaponEnabledByLabel('ARMAMB_GUN', true)
+	end,
+
+	Weapons = {
+		ARMAMB_GUN = Class(TAweapon) {
+			OnWeaponFired = function(self)
+				TAweapon.OnWeaponFired(self)	
+			end,
+
+			PlayFxWeaponUnpackSequence = function(self)
+				self.unit.damageReduction = 1
+				TAweapon.PlayFxWeaponUnpackSequence(self)
+			end,	
+
+			PlayFxWeaponPackSequence = function(self)
+				self.unit.damageReduction = 0.28
+				TAweapon.PlayFxWeaponPackSequence(self)
+			end,	
+		},
+	},
+}
+
+TypeClass = CORVIPE

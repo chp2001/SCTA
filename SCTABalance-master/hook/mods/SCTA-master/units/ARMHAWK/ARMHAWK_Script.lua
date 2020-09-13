a77b78917dbf6d8e216ea1d0c245bdc566e307e1
Dragun <poyosh2@aol.com>
2020-09-13 19:24:00 -0400

    Confused

--- SCTABalance-master/hook/mods/SCTA-master/units/ARMHAWK/ARMHAWK_Script.lua
+++
@@ -1,8 +0,0 @@
-local oldARMHAWK = ARMHAWK
-ARMHAWK = Class(oldARMHAWK) {
-	OnStopBeingBuilt = function(self)
-		self:SetMaintenanceConsumptionActive()
-		oldARMHAWK.OnStopBeingBuilt(self)
-end
-}
-TypeClass = ARMHAWK

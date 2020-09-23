7eeec53224ca5d237978aa47d16823d93e86d05e
Dragun <poyosh2@aol.com>
2020-09-22 20:42:32 -0400

    WE HAVE SPIDERS

---
+++ hook/lua/footprints.lua
@@ -0,0 +1,11 @@
+LAND = 0x01
+SEABED = 0x02
+SUB = 0x04
+WATER = 0x08
+AIR = 0x10
+ORBIT = 0x20
+
+
+SpecFootprints {
+    { Name = 'Spider1x1',   SizeX=1,  SizeZ=1,  Caps=LAND, MaxWaterDepth=1.5, MaxSlope=1.5, Flags=0 },
+}

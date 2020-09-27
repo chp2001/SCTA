1929c7919d4053997c8387be885768167009b100
Dragun <poyosh2@aol.com>
2020-09-27 16:19:15 -0400

    updates from sunday games

--- units/CORCOM/CORCOM_script.lua
+++ units/CORCOM/CORCOM_script.lua
@@ -23,14 +23,22 @@ CORCOM = Class(TAconstructor) {
 		CORE_DISINTEGRATOR = Class(TAweapon) {
 			OnWeaponFired = function(self)
 				TAweapon.OnWeaponFired(self)
-				
+				self:ForkThread(self.PauseOvercharge)
 				self.unit:SetWeaponEnabledByLabel('CORE_DISINTEGRATOR', true)
 			end,
 
 		        OnLostTarget = function(self)
 				self.unit:SetWeaponEnabledByLabel('CORE_DISINTEGRATOR', true)
 				TAweapon.OnLostTarget(self)
-		        end,
+				end,
+				
+				PauseOvercharge = function(self)
+					if not self.unit:IsOverchargePaused() then
+						self.unit:SetOverchargePaused(true)
+						WaitSeconds(1/self:GetBlueprint().RateOfFire)
+						self.unit:SetOverchargePaused(false)
+					end
+				end,
 		},
 		DeathWeapon = Class(TACommanderDeathWeapon) {},
 	},

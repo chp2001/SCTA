005904473f76a8ef3404c8c2029e7fee4e36bb2d
Dragun <poyosh2@aol.com>
2020-09-28 18:52:21 -0400

    Adjustments, pyro, and otherwise

--- units/CORSPY/CORSPY_Script.lua
+++ units/CORSPY/CORSPY_Script.lua
@@ -4,10 +4,43 @@
 #Script created by Raevn
 
 local TAWalking = import('/mods/SCTA-master/lua/TAWalking.lua').TAWalking
-local TAweapon = import('/mods/SCTA-master/lua/TAweapon.lua').TAweapon
-
 
 CORSPY = Class(TAWalking) {
+    OnStopBeingBuilt = function(self, builder, layer)
+		TAWalking.OnStopBeingBuilt(self, builder, layer)
+		self.Spinners = {
+			fork = CreateRotator(self, 'jam', 'y', nil, 0, 0, 0),
+		}
+		for k, v in self.Spinners do
+			self.Trash:Add(v)
+		end
+		self:SetMaintenanceConsumptionInactive()
+        self:SetScriptBit('RULEUTC_IntelToggle', true)
+		self:RequestRefreshUI()
+    end,
+
+	OnIntelDisabled = function(self)
+		self:SetMesh(self:GetBlueprint().Display.MeshBlueprint, true)
+	end,
+
+	OnIntelEnabled = function(self)
+		self:SetMesh('/mods/SCTA-master/units/CORSPY/CORSPY_cloak_mesh', true)
+	end,
+
+	OnScriptBitSet = function(self, bit)
+		if bit == 8 then
+			self.Spinners.fork:SetSpeed(0)
+		end
+		TAWalking.OnScriptBitSet(self, bit)
+	end,
+
+
+	OnScriptBitClear = function(self, bit)
+		if bit == 8 then
+			self.Spinners.fork:SetSpeed(100)
+		end
+		TAWalking.OnScriptBitClear(self, bit)
+	end,
 }
 
 TypeClass = CORSPY

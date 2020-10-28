fe6dc41182211f322b37cc507b13e72caf06cda8
Dragun <poyosh2@aol.com>
2020-10-28 17:38:25 -0400

    CorSpec BP setup

--- units/CORSPEC/CORSPEC_script.lua
+++ units/CORSPEC/CORSPEC_script.lua
@@ -9,25 +9,23 @@ local TAWalking = import('/mods/SCTA-master/lua/TAWalking.lua').TAWalking
 CORSPEC = Class(TAWalking) {
 	OnCreate = function(self)
 		TAWalking.OnCreate(self)
-
-		self.Spinners = {
-			
-		}
-		for k, v in self.Spinners do
-			self.Trash:Add(v)
-		end
+		self.AnimManip = CreateAnimator(self)
+		self.Trash:Add(self.AnimManip)
 	end,
 
 	Close = function(self)
 		self:PlayUnitSound('Deactivate')
-
-		
+		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationUnpack)
+		self.AnimManip:SetRate(-1 * (self:GetBlueprint().Display.AnimationUnpackRate or 0.2))
+        WaitFor(self.AnimManip)
+		self:SetMaintenanceConsumptionInactive()
 	end,
 
 	Open = function(self)
+		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationUnpack)
+		self.AnimManip:SetRate(1 * (self:GetBlueprint().Display.AnimationUnpackRate or 0.2))
+		self:SetMaintenanceConsumptionActive()
 		self:PlayUnitSound('Activate')
-
-		
 	end,
 
 	OnIntelDisabled = function(self)

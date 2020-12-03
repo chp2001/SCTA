b406fdc8a992aee34a17c5284063d69408195de3
Dragun <poyosh2@aol.com>
2020-12-03 13:49:34 -0500

    NavalRework

--- SCTABalance-master/hook/mods/SCTA-master/units/ARMSONAR/ARMSONAR_script.lua
+++ SCTABalance-master/hook/mods/SCTA-master/units/ARMSONAR/ARMSONAR_script.lua
@@ -1,5 +1,4 @@
 local oldARMSONAR = ARMSONAR
-local TAutils = import('/mods/SCTA-master/lua/TAutils.lua')
 
 ARMSONAR = Class(oldARMSONAR) {
 	OnStopBuild = function(self, unitBeingBuilt)
@@ -9,28 +8,6 @@ ARMSONAR = Class(oldARMSONAR) {
 			 self:Destroy()
 		 end
 	 end,
-
-	 OnStopBeingBuilt = function(self,builder,layer)
-		oldARMSONAR.OnStopBeingBuilt(self,builder,layer)
-		self:PlayUnitSound('Activate')
-		TAutils.registerTargetingFacility(self:GetArmy())
-	end,
-
-	OnScriptBitSet = function(self, bit)
-		if bit == 3 then
-			self:PlayUnitSound('Deactivate')
-			TAutils.unregisterTargetingFacility(self:GetArmy())
-		end
-		oldARMSONAR.OnScriptBitSet(self, bit)
-	end,
-
-	OnScriptBitClear = function(self, bit)
-		if bit == 3 then
-			self:PlayUnitSound('Activate')
-			TAutils.registerTargetingFacility(self:GetArmy())
-		end
-		oldARMSONAR.OnScriptBitClear(self, bit)
-	end,
 }
 
 TypeClass = ARMSONAR
--- SCTABalance-master/hook/mods/SCTA-master/units/CORSONAR/CORSONAR_script.lua
+++ SCTABalance-master/hook/mods/SCTA-master/units/CORSONAR/CORSONAR_script.lua
@@ -1,5 +1,4 @@
 local oldCORSONAR = CORSONAR
-local TAutils = import('/mods/SCTA-master/lua/TAutils.lua')
 
 CORSONAR = Class(oldCORSONAR) {
 	OnStopBuild = function(self, unitBeingBuilt)
@@ -9,28 +8,6 @@ CORSONAR = Class(oldCORSONAR) {
 			 self:Destroy()
 		 end
 	 end,
-
-	 OnStopBeingBuilt = function(self,builder,layer)
-		oldCORSONAR.OnStopBeingBuilt(self,builder,layer)
-		self:PlayUnitSound('Activate')
-		TAutils.registerTargetingFacility(self:GetArmy())
-	end,
-
-	OnScriptBitSet = function(self, bit)
-		if bit == 3 then
-			self:PlayUnitSound('Deactivate')
-			TAutils.unregisterTargetingFacility(self:GetArmy())
-		end
-		oldCORSONAR.OnScriptBitSet(self, bit)
-	end,
-
-	OnScriptBitClear = function(self, bit)
-		if bit == 3 then
-			self:PlayUnitSound('Activate')
-			TAutils.registerTargetingFacility(self:GetArmy())
-		end
-		oldCORSONAR.OnScriptBitClear(self, bit)
-	end,
 }
 
 TypeClass = CORSONAR

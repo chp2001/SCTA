871e1670f9d64bcbf8170fcf4e73c7b3d283cce8
Dragun <poyosh2@aol.com>
2020-12-15 11:55:07 -0500

    Fact

---
+++ SCTABalance/mod_info.lua
@@ -0,0 +1,15 @@
+name = "SCTA Test Rebalance Mod"
+version = 22
+copyright = "Dragun123@2020"
+description = "Makes TA Factions from SCTA Mod cost in line with modern factions. With an exceptional amount of help from Balthazar with actual coding process. And thank you Odin, Silene and Hell for helping to test."
+author = "Dragun101"
+uid = "8g3go543-22e9-605f-5g46-b17h1c9726z9"
+selectable = true
+enabled = true
+icon = "/mods/SCTA-master/SCTABalance/SCTAREBalance.png"
+exclusive = false
+ui_only = false
+requires = {"8E704ECC-12EC-11EA-A6CA-D6DA930038A2"}
+conflicts = {}
+before = {}
+after = {"8E704ECC-12EC-11EA-A6CA-D6DA930038A2"}

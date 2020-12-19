5f31e6fb2fa7882e5f3f7feb98f3a34e81bb51ce
Dragun <poyosh2@aol.com>
2020-12-19 10:29:04 -0500

    CarriersWorking

--- lua/TAFactory.lua
+++ lua/TAFactory.lua
@@ -1,4 +1,5 @@
 local FactoryUnit = import('/lua/defaultunits.lua').FactoryUnit
+local AircraftCarrier = import('/lua/defaultunits.lua').AircraftCarrier
 local Unit = import('/lua/sim/Unit.lua').Unit
 local TAutils = import('/mods/SCTA-master/lua/TAutils.lua')
 local EffectUtil = import('/lua/EffectUtilities.lua')
@@ -30,4 +31,84 @@ TAFactory = Class(FactoryUnit) {
 		end,
 	}
 
-TypeClass = TAFactory
+TACarrier = Class(AircraftCarrier) {
+
+    CreateBuildEffects = function(self, unitBeingBuilt, order)
+        TAutils.CreateTAFactBuildingEffects( self, unitBeingBuilt, self.BuildEffectBones, self.BuildEffectsBag )
+    end,
+	
+	BuildAttachBone = 'Attachpoint_Med_02',
+
+    OnFailedToBuild = function(self)
+        AircraftCarrier.OnFailedToBuild(self)
+        ChangeState(self, self.IdleState)
+    end,
+
+    IdleState = State {
+        Main = function(self)
+            self:DetachAll(self.BuildAttachBone)
+            self:SetBusy(false)
+        end,
+
+		OnStartBuild = function(self, unitBuilding, order )
+			self:Open()
+			self.unitBeingBuilt = unitBuilding
+			ForkThread(self.FactoryStartBuild, self, unitBuilding, order )
+		end,
+
+		FactoryStartBuild = function(self, unitBuilding, order )
+			WaitFor(self.AnimManip)
+            AircraftCarrier.OnStartBuild(self, unitBuilding, order)
+            ChangeState(self, self.BuildingState)
+		end,
+		
+		Open = function(self)
+			self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationBuild)
+			self.AnimManip:SetRate(1 * (self:GetBlueprint().Display.AnimationBuildRate or 0.2))
+		end,
+	
+    },
+
+    BuildingState = State {
+        Main = function(self)
+            local unitBuilding = self.unitBeingBuilt
+            self:SetBusy(true)
+            local bone = self.BuildAttachBone
+            self:DetachAll(bone)
+            unitBuilding:HideBone(0, true)
+            self.UnitDoneBeingBuilt = false
+        end,
+
+        OnStopBuild = function(self, unitBeingBuilt)
+            AircraftCarrier.OnStopBuild(self, unitBeingBuilt)
+            self:Close()
+            ChangeState(self, self.FinishedBuildingState)
+        end,
+		
+		Close = function(self)
+			self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationBuild)
+			self.AnimManip:SetRate(-1 * (self:GetBlueprint().Display.AnimationBuildRate or 0.2))
+			self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationPower)
+			self.AnimManip:SetRate(1 * (self:GetBlueprint().Display.AnimationPowerRate or 0.2))
+		end,
+    },
+
+    FinishedBuildingState = State {
+        Main = function(self)
+            self:SetBusy(true)
+            local unitBuilding = self.unitBeingBuilt
+            unitBuilding:DetachFrom(true)
+            self:DetachAll(self.BuildAttachBone)
+            if self:TransportHasAvailableStorage() then
+                self:AddUnitToStorage(unitBuilding)
+            else
+                local worldPos = self:CalculateWorldPositionFromRelative({0, 0, -20})
+                IssueMoveOffFactory({unitBuilding}, worldPos)
+                unitBuilding:ShowBone(0,true)
+            end
+            self:SetBusy(false)
+			self:RequestRefreshUI()
+			ChangeState(self, self.IdleState)
+        end,
+	},
+}
--- units/CORCARRY/CORCARRY_Script.lua
+++ units/CORCARRY/CORCARRY_Script.lua
@@ -3,11 +3,11 @@
 #
 #Script created by Raevn
 
-local TAFactory = import('/mods/SCTA-master/lua/TAFactory.lua').TAFactory
+local TACarrier = import('/mods/SCTA-master/lua/TAFactory.lua').TACarrier
 
-CORCARRY = Class(TAFactory) {
+CORCARRY = Class(TACarrier) {
 	OnCreate = function(self)
-		TAFactory.OnCreate(self)
+		TACarrier.OnCreate(self)
 		self.Spinners = {
 			dish = CreateRotator(self, 'dish', 'y', nil, 0, 0, 0),
 		}
@@ -16,41 +16,32 @@ CORCARRY = Class(TAFactory) {
 		end
 		self.AnimManip = CreateAnimator(self)
 		self.Trash:Add(self.AnimManip)
-	end,
+    end,
+    
+    OnIntelDisabled = function(self)
+		self.Spinners.dish:SetSpeed(0)
+		self:SetMaintenanceConsumptionInactive()
+	    TACarrier.OnIntelDisabled(self)
+end,
+
+
+    OnIntelEnabled = function(self)
+		self.Spinners.dish:SetSpeed(60)
+		self:SetMaintenanceConsumptionActive()
+	    TACarrier.OnIntelEnabled(self)
+end,
 
 	OnStopBeingBuilt = function(self,builder,layer)
-		TAFactory.OnStopBeingBuilt(self,builder,layer)
+		TACarrier.OnStopBeingBuilt(self,builder,layer)
 		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationPower)
 		self.AnimManip:SetRate(1 * (self:GetBlueprint().Display.AnimationPowerRate or 0.2))
 		--SPIN dish around y-axis SPEED <60.01>
 		self.Spinners.dish:SetSpeed(60)
 		self:SetMaintenanceConsumptionActive()
-	end,
-
-	Open = function(self)
-		TAFactory.Open(self)
-		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationBuild)
-		self.AnimManip:SetRate(1 * (self:GetBlueprint().Display.AnimationBuildRate or 0.2))
-	end,
-
-	Close = function(self)
-		TAFactory.Close(self)
-		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationBuild)
-		self.AnimManip:SetRate(-1 * (self:GetBlueprint().Display.AnimationBuildRate or 0.2))
-	end,
-
-	OnIntelDisabled = function(self)
-		self.Spinners.dish:SetSpeed(0)
-		self:SetMaintenanceConsumptionInactive()
-	TAFactory.OnIntelDisabled(self)
-end,
-
-
-OnIntelEnabled = function(self)
-		self.Spinners.dish:SetSpeed(60)
-		self:SetMaintenanceConsumptionActive()
-	TAFactory.OnIntelEnabled(self)
-end,
+        ChangeState(self, self.IdleState)
+    end,
 }
+
+
 
 TypeClass = CORCARRY

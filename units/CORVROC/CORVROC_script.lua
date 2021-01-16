4400ec9c3debd4c233286f18cb3822bd49b5401f
Dragun <poyosh2@aol.com>
2021-01-16 09:44:25 -0500

    vroc

--- units/CORVROC/CORVROC_script.lua
+++ units/CORVROC/CORVROC_script.lua
@@ -3,57 +3,24 @@
 #
 #Script created by Raevn
 
-local TAunit = import('/mods/SCTA-master/lua/TAunit.lua').TAunit
+local TATreads = import('/mods/SCTA-master/lua/TAMotion.lua').TATreads
 local TAweapon = import('/mods/SCTA-master/lua/TAweapon.lua').TAweapon
 
-CORVROC = Class(TAunit) {
-	
-	OnCreate = function(self)
-		TAunit.OnCreate(self)
-		self.Spinners = {
-			cradle = CreateRotator(self, 'cradle', 'x', nil, 0, 0, 0),
-		}
-		for k, v in self.Spinners do
-			self.Trash:Add(v)
-		end
-	end,
-
+CORVROC = Class(TATreads) {
 	Weapons = {
 		CORTRUCK_ROCKET = Class(TAweapon) {
 
     			PlayFxRackReloadSequence = function(self)
-
-				--TURN cradle to x-axis <0> SPEED <45.09>;
-				self.unit.Spinners.cradle:SetGoal(0)
-				self.unit.Spinners.cradle:SetSpeed(45)
-
 				self.unit:ShowBone('dummy', true)
-
-				--TURN cradle to x-axis <-90.00> SPEED <45.39>;
-				self.unit.Spinners.cradle:SetGoal(-90)
-				self.unit.Spinners.cradle:SetSpeed(45)
-
-				WaitFor(self.unit.Spinners.cradle)
-
 				TAweapon.PlayFxRackReloadSequence(self)
 			end,
 
 			PlayFxWeaponUnpackSequence = function(self)
-
-				--TURN cradle to x-axis <-90.00> SPEED <45.39>;
-				self.unit.Spinners.cradle:SetGoal(-90)
-				self.unit.Spinners.cradle:SetSpeed(45)
-
-				WaitFor(self.unit.Spinners.cradle)
-
+				self.unit:ShowBone('dummy', true)
 				TAweapon.PlayFxWeaponUnpackSequence(self)
 			end,
 
 			PlayFxWeaponPackSequence = function(self)
-				--TURN cradle to x-axis <0> SPEED <45.09>;
-				self.unit.Spinners.cradle:SetGoal(0)
-				self.unit.Spinners.cradle:SetSpeed(45)
-
 				TAweapon.PlayFxWeaponPackSequence(self)
 			end,
 		},

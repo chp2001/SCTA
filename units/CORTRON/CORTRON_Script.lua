4f9df78e286b4fe673e01a34ba40f877f4a4d6cb
Dragun <poyosh2@aol.com>
2021-01-27 10:29:04 -0500

    stuff

--- units/CORTRON/CORTRON_Script.lua
+++ units/CORTRON/CORTRON_Script.lua
@@ -5,7 +5,6 @@
 
 local TAnoassistbuild = import('/mods/SCTA-master/lua/TAStructure.lua').TAnoassistbuild
 local TIFStrategicMissileWeapon = import('/lua/terranweapons.lua').TIFStrategicMissileWeapon
-local TAutils = import('/mods/SCTA-master/lua/TAutils.lua')
 
 CORTRON = Class(TAnoassistbuild) {
 	OnCreate = function(self)

6dc74016a088b06e466441fdd5493538b845f6d1
Dragun <poyosh2@aol.com>
2021-01-29 19:33:51 -0500

    ArmsnipeFark

--- units/ARMSNIPE/ARMSNIPE_script.lua
+++ units/ARMSNIPE/ARMSNIPE_script.lua
@@ -9,18 +9,6 @@ local TAweapon = import('/mods/SCTA-master/lua/TAweapon.lua').TAweapon
 ARMSNIPE = Class(TAWalking) {
     OnStopBeingBuilt = function(self, builder, layer)
 		TAWalking.OnStopBeingBuilt(self, builder, layer)
-		self.Spinners = {
-			lgunbase = CreateRotator(self, 'gun', 'x', nil, 0, 0, 0),
-		}
-		self.Sliders = {
-			sleave = CreateSlider(self, 'sleave'),
-		}
-		for k, v in self.Sliders do
-			self.Trash:Add(v)
-		end
-		for k, v in self.Spinners do
-			self.Trash:Add(v)
-		end
         self:SetMaintenanceConsumptionInactive()
         self:SetScriptBit('RULEUTC_CloakToggle', true)
         self:RequestRefreshUI()
@@ -31,27 +19,6 @@ ARMSNIPE = Class(TAWalking) {
 			OnWeaponFired = function(self)
 				TAweapon.OnWeaponFired(self)
 			end,
-
-			PlayFxWeaponUnpackSequence = function(self)
-				TAweapon.PlayFxWeaponUnpackSequence(self)
-				self.unit.Spinners.lgunbase:SetGoal(90)
-				self.unit.Spinners.lgunbase:SetSpeed(90)
-
-				--MOVE barrel to z-axis <0> SPEED <22.00>;
-				self.unit.Sliders.sleave:SetGoal(0,0,1)
-				self.unit.Sliders.sleave:SetSpeed(1)
-			end,	
-	
-			PlayFxWeaponPackSequence = function(self)
-				TAweapon.PlayFxWeaponPackSequence(self)
-				self.unit.Spinners.lgunbase:SetGoal(0)
-				self.unit.Spinners.lgunbase:SetSpeed(90)
-
-				
-				--MOVE barrel to z-axis <0> SPEED <22.00>;
-				self.unit.Sliders.sleave:SetGoal(0,0,0)
-				self.unit.Sliders.sleave:SetSpeed(1)
-			end,		
 		},
 	},
 }

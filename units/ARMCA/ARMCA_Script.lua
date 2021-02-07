c3cbcb5a792cf98bc05af77484d04b9101637c61
Dragun <poyosh2@aol.com>
2021-02-07 09:38:57 -0500

    armca

--- units/ARMCA/ARMCA_Script.lua
+++ units/ARMCA/ARMCA_Script.lua
@@ -9,13 +9,8 @@ local TAutils = import('/mods/SCTA-master/lua/TAutils.lua')
 ARMCA = Class(TAAirConstructor) {
 	OnCreate = function(self)
 		TAAirConstructor.OnCreate(self)
-		self.Sliders = {
-			wing1 = CreateSlider(self, 'rwingb'),
-			wing2 = CreateSlider(self, 'lwingb'),
-		}
-		for k, v in self.Sliders do
-			self.Trash:Add(v)
-		end
+		self.AnimManip = CreateAnimator(self)
+		self.Trash:Add(self.AnimManip)
 	end,
 
 	OnMotionVertEventChange = function(self, new, old )
@@ -35,22 +30,14 @@ ARMCA = Class(TAAirConstructor) {
 
 	OpenWings = function(self)
 		--MOVE wing1 to x-axis <5.59> SPEED <5.00>;
-		self.Sliders.wing1:SetGoal(-6.25,0,0)
-		self.Sliders.wing1:SetSpeed(4)
-
-		--MOVE wing2 to x-axis <-5.65> SPEED <5.00>;
-		self.Sliders.wing2:SetGoal(6.25,0,0)
-		self.Sliders.wing2:SetSpeed(4)
+		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationWing)
+		self.AnimManip:SetRate(1 * (self:GetBlueprint().Display.AnimationWingRate or 0.2))
 	end,
 
 	CloseWings = function(self)
 		--MOVE wing1 to x-axis <0> SPEED <4.00>;
-		self.Sliders.wing1:SetGoal(0,0,0)
-		self.Sliders.wing1:SetSpeed(4)
-
-		--MOVE wing2 to x-axis <0> SPEED <6.00>;
-		self.Sliders.wing2:SetGoal(0,0,0)
-		self.Sliders.wing2:SetSpeed(4)
+		self.AnimManip:PlayAnim(self:GetBlueprint().Display.AnimationWing)
+		self.AnimManip:SetRate(-1 * (self:GetBlueprint().Display.AnimationWingRate or 0.2))
 	end,
 }
 

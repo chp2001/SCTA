d515322e5221864e339d5c97fa556be4a0b38960
Dragun <poyosh2@aol.com>
2021-02-13 14:08:48 -0500

    Amph

--- hook/lua/footprints.lua
+++ hook/lua/footprints.lua
@@ -9,4 +9,5 @@ ORBIT = 0x20
 SpecFootprints {
     { Name = 'Spider1x2', SizeX = 1,  SizeZ = 2,  Caps=LAND, MaxWaterDepth=0.05,  MaxSlope=1.5, Flags=0 },
     { Name = 'Commander1x2',   SizeX = 1,  SizeZ = 2,  Caps=LAND|SEABED, MaxWaterDepth=15, MaxSlope=1.25, Flags=0 },
+    { Name = 'Hover1x2',   SizeX = 1,  SizeZ = 2,  Caps=LAND|WATER, MaxWaterDepth=1, MinWaterDepth=0.1, MaxSlope=1.25, Flags=0 },
 }

fd3f7d9c08eaec04e2bc08cc16d1885f8cd10dd1
Dragun <poyosh2@aol.com>
2021-03-14 16:34:25 -0400

    DONE

--- hook/lua/ui/game/taunt.lua
+++ hook/lua/ui/game/taunt.lua
@@ -1,8 +1,8 @@
 do
 	taunts = table.cat(taunts, {
-	{text = '[Commander]: Your forces have gone to a better place', bank = 'TA_Music', cue = 'Battle'},
+	{text = '[Commander]: Our foes will be obliterated', bank = 'XGG', cue = 'Computer_Computer_MapExpansion_01380'},
 	{text = '[Commander]: The Enemy Vermin have been exterminated', bank = 'TA_Music', cue = 'Building'},
-	{text = '[Commander]: The opposition will be obliterated', bank = 'TA_Music', cue = 'Battle'},
+	{text = '[Commander]: Prepare to be sent to a better place', bank = 'TA_Music', cue = 'Battle'},
 } )
 
 end

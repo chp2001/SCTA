5b42462fde45dccf8491b7bf0417b7eb49c58892
Dragun <poyosh2@aol.com>
2021-03-17 10:58:17 -0400

    Rel

--- lua/TAutils.lua
+++ lua/TAutils.lua
@@ -261,11 +261,24 @@ function TAAIGetReclaimablesAroundLocation(aiBrain, locationType)
         return false
     end
 
-    local x1 = position[1] - radius * 3
-    local x2 = position[1] + radius * 3
-    local z1 = position[3] - radius * 3
-    local z2 = position[3] + radius * 3
+    local x1 = position[1] - radius * 2
+    local x2 = position[1] + radius * 2
+    local z1 = position[3] - radius * 2
+    local z2 = position[3] + radius * 2
     local rect = Rect(x1, z1, x2, z2)
 
     return AIUtils.GetReclaimablesInRect(rect)
+end
+
+
+function GetDirectionInDegrees( v1, v2 )
+    local SCTAACOS = math.acos
+    local PI = math.pi
+    local vec = GetDirectionVector( v1, v2)
+    
+    if vec[1] >= 0 then
+        return SCTACOS(vec[3]) * (360/(PI*2))
+    end
+    
+    return 360 - (SCTACOS(vec[3]) * (360/(PI*2)))
 end

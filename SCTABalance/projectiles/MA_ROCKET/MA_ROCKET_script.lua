83b818b338efb10c12fc2fed3f3583c931146d3c
Dragun <poyosh2@aol.com>
2021-03-27 16:56:03 -0400

    OkayDone

---
+++ SCTABalance/projectiles/MA_ROCKET/MA_ROCKET_script.lua
@@ -0,0 +1,10 @@
+#ARM Merl Rocket
+#ARMTRUCK_ROCKET
+#
+#Script created by Raevn
+
+local TAMissileProjectile = import('/mods/SCTA-master/lua/TAprojectiles.lua').TAMissileProjectile
+
+MA_ROCKET = Class(TAMissileProjectile) {}
+
+TypeClass = MA_ROCKET

1b46c855b900b1e4c7b2dd40e44bb9c8f8382ffc
Dragun <poyosh2@aol.com>
2021-03-30 16:22:13 -0400

    k

--- hook/lua/ui/game/taunt.lua
+++ hook/lua/ui/game/taunt.lua
@@ -2,7 +2,7 @@ do
 	taunts = table.cat(taunts, {
 	{text = '[Commander]: Our foes will be obliterated', bank = 'XGG', cue = 'XGG_Computer_CV01_04724'},
 	{text = '[Commander]: The Enemy Vermin has been exterminated', bank = 'XGG', cue = 'XGG_Computer__04719'},
-	{text = '[Commander]: Prepare to be sent to a better place', bank = 'XGG', cue = 'XGG_Computer_CV01_04730'},
+	{text = '[Commander]: Prepare to be sent to a better place', bank = 'XGG', cue = 'XGG_Computer_CV01_04734'},
 } )
 
 end

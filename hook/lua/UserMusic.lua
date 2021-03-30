eb2a7c8e44b2f8c1ce189a0fe977601770c3ab45
Dragun <poyosh2@aol.com>
2021-03-30 16:18:41 -0400

    k

--- hook/lua/UserMusic.lua
+++ hook/lua/UserMusic.lua
@@ -7,7 +7,8 @@
 --#****************************************************************************
 --# Config options
 --#****************************************************************************
-
+local TAStartBattleMusic = StartBattleMusic
+local TAStartPeaceMusic = StartPeaceMusic
 -- List of battle cues to cycle through
 local TABattleCues = {
     Sound({Cue = 'Battle', Bank = 'TA_Music'}),
@@ -18,72 +19,24 @@ local TAPeaceCues = {
     Sound({Cue = 'Building', Bank = 'TA_Music'}),
 }
 
-function StartTAMusic()
-    TAPlayMusic(TABattleCues[BattleCueIndex], 0)
-end
-
-
-function TAPlayMusic(cue, delay)
-    if(musicThread) then KillThread(musicThread) end
-
-    musicThread = ForkThread(
-        function()
-            local delay = delay or 0
-
-            if currentMusic then
-                StopSound(currentMusic, delay == 0)
-                if delay > 0 then
-                    WaitFor(currentMusic)
-                    WaitSeconds(delay)
-                end
-
-                currentMusic = nil
-            end
-
-
-            currentMusic = PlaySound(cue)
-            WaitSeconds(10)
-            StopSound(currentMusic)
-            StartPeaceMusic()
-            if paused then
-                WaitSeconds(1)
-                PauseSound("Music", true)
-            end
-        end)
-end
-
-
+local TABattleCueIndex = 1
+local TAPeaceCueIndex = 1
 function StartBattleMusic()
-    BattleStart = GameTick()
     local coinFlip = math.random(2)
     if coinFlip == 1 then
-    PlayMusic(TABattleCues[BattleCueIndex], 0)
-    elseif coinFlip == 2 then
-    PlayMusic(BattleCues[BattleCueIndex], 0) -- immediately
+    PlayMusic(TABattleCues[TABattleCueIndex], 0)
+    TABattleCueIndex = math.mod(TABattleCueIndex,table.getn(TABattleCues))
+else
+    TAStartBattleMusic()
     end
-    BattleCueIndex = math.mod(BattleCueIndex,table.getn(BattleCues)) + 1
-
-    if battleWatch then KillThread(battleWatch) end
-    battleWatch = ForkThread(
-        function ()
-            while GameTick() - LastBattleNotify < PeaceTimer do
-                WaitSeconds(1)
-            end
-
-            StartPeaceMusic()
-        end
-)
 end
 
 function StartPeaceMusic()
-    BattleStart = 0
-    BattleEventCounter = 0
-    LastBattleNotify = GameTick()
     local coinFlip = math.random(2)
     if coinFlip == 1 then
-    PlayMusic(TAPeaceCues[PeaceCueIndex], 3)
-    elseif coinFlip == 2 then
-    PlayMusic(PeaceCues[PeaceCueIndex], 3)
+    PlayMusic(TAPeaceCues[TAPeaceCueIndex], 1)
+    TAPeaceCueIndex = math.mod(TAPeaceCueIndex, table.getsize(TAPeaceCues))
+    else
+    TAStartPeaceMusic()
     end
-    PeaceCueIndex = math.mod(PeaceCueIndex, table.getsize(PeaceCues)) + 1
 end
--- hook/lua/ui/game/gamemain.lua
+++
@@ -1,6 +0,0 @@
-TAFirstUpdate = OnFirstUpdate
-
-function OnFirstUpdate()
-    TAFirstUpdate()
-    import('/lua/UserMusic.lua').StartTAMusic()
-end

3f12e1f1de66439a088725c2d2facb067c856cc2
Dragun <poyosh2@aol.com>
2021-05-28 10:16:07 -0400

    WindGenerators

--- lua/TAWeather.lua
+++ lua/TAWeather.lua
@@ -100,39 +100,37 @@ local WindEnergyRange = false
 TAWin = Class(TAStructure) 
 {
     OnStopBeingBuilt = function(self,builder,layer)
-    TAStructure.OnStopBeingBuilt(self,builder,layer)
-    self:SetProductionPerSecondEnergy(0)
-    if not WindEnergyMin and not WindEnergyRange then
+        TAStructure.OnStopBeingBuilt(self,builder,layer)
+        self:SetProductionPerSecondEnergy(0)
+        if not WindEnergyMin and not WindEnergyRange then
         --LOG("Defining wind turbine energy output value range.")
-        local bp = self:GetBlueprint().Economy
-        local mean = bp.ProductionPerSecondEnergy or 17.5
-        local min = bp.ProductionPerSecondEnergyMin or 5
-        local max = bp.ProductionPerSecondEnergyMax or 30
-        if (min + max) / 2 == mean then
+            local bp = self:GetBlueprint().Economy
+            local mean = bp.ProductionPerSecondEnergy or 17.5
+            local min = bp.ProductionPerSecondEnergyMin or 5
+            local max = bp.ProductionPerSecondEnergyMax or 30
+                if (min + max) / 2 == mean then
             --Then nothing has messed with the numbers, or something messed with all of them.
-            WindEnergyMin = min
-            WindEnergyRange = max - min
-        else
+                WindEnergyMin = min
+                WindEnergyRange = max - min
+            else
             --Something has messed with the numbers, and we should move to match.
-            local mult = mean / 17.5
-            WindEnergyMin = min * mult
-            WindEnergyRange = (max - min) * mult
+                local mult = mean / 17.5
+                WindEnergyMin = min * mult
+                WindEnergyRange = (max - min) * mult
+            end
         end
-    end
     ------------------------------------------------------------------------
     -- Run the thread
     ------------------------------------------------------------------------
     self:ForkThread(SyncroniseThread,30,self.OnWeatherInterval,self)
-end,
+    self.Spinners.post:SetSpeed(35)
+    self.Spinners.blades:SetSpeed(50)
+    end,
 
-OnWeatherInterval = function(self)
+    OnWeatherInterval = function(self)
     ---LOG('Wind Being Ran')
-   self:SetProductionPerSecondEnergy (
-       (WindEnergyMin + WindEnergyRange * ScenarioInfo.WindStats.Power)
-   )
-end,
-
-        OnKilled = function(self, instigator, type, overKillRatio)
-           TAStructure.OnKilled(self, instigator, type, overKillRatio)
-        end,
+    self:SetProductionPerSecondEnergy (
+        (WindEnergyMin + WindEnergyRange * ScenarioInfo.WindStats.Power)
+    )
+    end,
     }
--- units/ARMWIN/ARMWIN_Script.lua
+++ units/ARMWIN/ARMWIN_Script.lua
@@ -11,17 +11,11 @@ ARMWIN = Class(TAWin) {
 		self:SetProductionPerSecondEnergy(0)
 		self.Spinners = {
 		   --CreateRotator(Win, bone, axis, [goal], [speed], [accel], [goalspeed])
-		   blades = CreateRotator(self, 'Blades', 'z', nil, 0, 0, 0),
-		   post = CreateRotator(self, 'Turret', 'y', nil, 0, 0, 0),
+		   blades = CreateRotator(self, 'Rotor', 'z', nil, 0, 0, 0),
+		   post = CreateRotator(self, 'Top', 'z', nil, 0, 0, 0),
 	   }
 	   	self.Trash:Add(self.Spinners.blades)
 		self.Trash:Add(self.Spinners.post)
-	end,
-
-	OnStopBeingBuilt = function(self,builder,layer)
-		TAWin.OnStopBeingBuilt(self,builder,layer)
-		self.Spinners.post:SetSpeed(35)
-		self.Spinners.blades:SetSpeed(50)
 	end,
 }
 

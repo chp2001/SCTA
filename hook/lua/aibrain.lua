bcfe4b06dbcbc10896c4fd16fa73a244b7a76ae5
chp2001 <chp.2001.jhp@gmail.com>
2021-06-30 16:14:42 -0400

    redoneformmanager

--- hook/lua/aibrain.lua
+++ hook/lua/aibrain.lua
@@ -1,41 +1,75 @@
 WARN('['..string.gsub(debug.getinfo(1).source, ".*\\(.*.lua)", "%1")..', line:'..debug.getinfo(1).currentline..'] * SCTAAI: offset aibrain.lua' )
---local TAReclaim = import('/mods/SCTA-master/lua/AI/TAEditors/TAAIUtils.lua')
 
 SCTAAIBrainClass = AIBrain
 AIBrain = Class(SCTAAIBrainClass) {
-        OnSpawnPreBuiltUnits = function(self)
-            if not self.SCTAAI then
-                return SCTAAIBrainClass.OnSpawnPreBuiltUnits(self)
+    FormManagerSCTA = function(self)
+        local aiBrain=self
+        LOG('BEGINNING SCTA FORMMANAGER')
+        if aiBrain.FormManagerSCTAI then return end
+        aiBrain.FormManagerSCTAI=CurrentThread()
+        local GetCurrentUnits = moho.aibrain_methods.GetCurrentUnits
+        if not aiBrain.SeaForm then
+            aiBrain.SeaForm=0
+            aiBrain.StructureForm=0
+            aiBrain.Other=0
+            aiBrain.LandForm=0
+            aiBrain.AirForm=0
+            aiBrain.Scout=0
+        end
+        local checks={
+            SeaForm=0,
+            StructureForm=0,
+            Other=0,
+            LandForm=0,
+            AirForm=0,
+            Scout=0,
+        }
+        while (aiBrain.Result~='defeat') do
+            if aiBrain.SeaForm < 1 and checks.SeaForm<1 then
+                aiBrain.SeaForm = GetCurrentUnits(aiBrain, (categories.NAVAL * categories.MOBILE) - categories.ENGINEER)
+                WaitTicks(1)
+                checks.SeaForm=checks.SeaForm+3
             end
-            local per = ScenarioInfo.ArmySetup[self.Name].AIPersonality
-            local resourceStructures = nil
-            local initialUnits = nil
-            local posX, posY = self:GetArmyStartPos()
-    
-            if string.find(per, 'arm') then
-                resourceStructures = {'armmex', 'armmex', 'armmex', 'armmex'}
-                initialUnits = {'armlab', 'armsolar', 'armsolar', 'armsolar', 'armsolar'}
-            else
-                resourceStructures = {'cormex', 'cormex', 'cormex', 'cormex'}
-                initialUnits = {'corvp', 'corsolar', 'corsolar', 'corsolar', 'corsolar'}
+            if aiBrain.StructureForm > 3 and checks.StructureForm<1 then
+                aiBrain.StructureForm = GetCurrentUnits(aiBrain, categories.STRUCTURE * (categories.CQUEMOV + categories.MASSFABRICATION))
+                WaitTicks(1)
+                checks.StructureForm=checks.StructureForm+2
             end
-    
-            if resourceStructures then
-                -- Place resource structures down
-                for k, v in resourceStructures do
-                    local unit = self:CreateResourceBuildingNearest(v, posX, posY)
-                end
+            if aiBrain.Other < 1 and checks.Other<1 then
+                aiBrain.Other = GetCurrentUnits(aiBrain, categories.EXPERIMENTAL * categories.MOBILE)
+                WaitTicks(1)
+                checks.Other=checks.Other+10
             end
-    
-            if initialUnits then
-                -- Place initial units down
-                for k, v in initialUnits do
-                    local unit = self:CreateUnitNearSpot(v, posX, posY)
-                end
+            if aiBrain.LandForm < 1 and checks.LandForm<1 then
+                aiBrain.LandForm = GetCurrentUnits(aiBrain, (categories.LAND * categories.MOBILE) - categories.ENGINEER - categories.SCOUT)
+                WaitTicks(1)
+                checks.LandForm=checks.LandForm+2
             end
-    
-            self.PreBuilt = true
-        end,
+            if aiBrain.AirForm < 1 and checks.AirForm<1 then
+                aiBrain.AirForm = GetCurrentUnits(aiBrain, (categories.AIR * categories.MOBILE) - categories.ENGINEER - categories.SCOUT)
+                WaitTicks(1)
+                checks.AirForm=checks.AirForm+3
+            end
+            if aiBrain.Scout < 1 and checks.Scout<1 then
+                aiBrain.Scout = GetCurrentUnits(aiBrain, (categories.armpw + categories.corgator + (categories.SCOUT + categories.AMPHIBIOUS) - categories.ENGINEER - categories.EXPERIMENTAL))
+                WaitTicks(1)
+                checks.Scout=checks.Scout+2
+            end
+            --[[LOG('formmanagerdata')
+            LOG(repr(aiBrain.SeaForm))
+            LOG(repr(aiBrain.StructureForm))
+            LOG(repr(aiBrain.Other))
+            LOG(repr(aiBrain.LandForm))
+            LOG(repr(aiBrain.AirForm))
+            LOG(repr(aiBrain.Scout))
+            LOG('checks '..repr(checks))]]
+            for k,v in checks do
+                checks[k]=checks[k]-1
+            end
+            --LOG('checks '..repr(checks))
+            WaitTicks(5)
+        end
+    end,
        
 
     AddBuilderManagers = function(self, position, radius, baseName, useCenter)
@@ -48,15 +82,15 @@ AIBrain = Class(SCTAAIBrainClass) {
              FactoryManager = FactoryManager.CreateFactoryBuilderManager(self, baseName, position, radius, useCenter),
              PlatoonFormManager = PlatoonFormManager.CreatePlatoonFormManager(self, baseName, position, radius, useCenter),
              EngineerManager = EngineerManager.CreateEngineerManager(self, baseName, position, radius),
-             BuilderHandles = {},
              MassConsumption = {
-                Resources = {Units = {}, Drain = 0, },
-                Units = {Units = {}, Drain = 0, },
-                Defenses = {Units = {}, Drain = 0, },
-                Upgrades = {Units = {}, Drain = 0, },
-                Engineers = {Units = {}, Drain = 0, },
-                TotalDrain = 0,
-            },
+                 Resources = {Units = {}, Drain = 0, },
+                 Units = {Units = {}, Drain = 0, },
+                 Defenses = {Units = {}, Drain = 0, },
+                 Upgrades = {Units = {}, Drain = 0, },
+                 Engineers = {Units = {}, Drain = 0, },
+                 TotalDrain = 0,
+             },
+             BuilderHandles = {},
              Position = position,
              BaseType = Scenario.MasterChain._MASTERCHAIN_.Markers[baseName].type or 'MAIN',
          }
@@ -65,12 +99,12 @@ AIBrain = Class(SCTAAIBrainClass) {
      end,
 
     OnCreateAI = function(self, planName)
-        SCTAAIBrainClass.OnCreateAI(self, planName) 
+        SCTAAIBrainClass.OnCreateAI(self, planName)
+        local per = ScenarioInfo.ArmySetup[self.Name].AIPersonality
         --LOG('Oncreate')
-        if string.find(ScenarioInfo.ArmySetup[self.Name].AIPersonality, 'scta') then
+        if string.find(per, 'scta') then
             --LOG('* AI-SCTA: This is SCTA')
             self.SCTAAI = true
-            --self.ForkThread(TAReclaim.MassFabManagerThreadSCTAI, self)
         end
     end,
 
@@ -135,4 +169,5 @@ AIBrain = Class(SCTAAIBrainClass) {
             return SCTAAIBrainClass.InitializeEconomyState(self)
         end
     end,
+
 }
--- hook/lua/sim/PlatoonFormManager.lua
+++ hook/lua/sim/PlatoonFormManager.lua
@@ -7,6 +7,9 @@ PlatoonFormManager = Class(SCTAPlatoonFormManager) {
             --LOG('*self.template2', brain.SCTAAI)
             return SCTAPlatoonFormManager.Create(self, brain, lType, location, radius)
         end
+        if not brain.FormManagerSCTAI then
+            ForkThread(brain.FormManagerSCTA,brain)
+        end
         BuilderManager.Create(self,brain)
         --LOG('IEXIST2')
         if not lType or not location or not radius then
@@ -51,6 +54,7 @@ PlatoonFormManager = Class(SCTAPlatoonFormManager) {
         end
         return newBuilder
     end,
+    
 
     ManagerLoopBody = function(self,builder,bType)
         if not self.Brain.SCTAAI then
@@ -60,63 +64,50 @@ PlatoonFormManager = Class(SCTAPlatoonFormManager) {
         BuilderManager.ManagerLoopBody(self,builder,bType)
         ---local builder = self:GetHighestBuilder(bType, {builder})
             --local pool = self.Brain:GetPlatoonUniquelyNamed('ArmyPool')
-            local GetCurrentUnits = moho.aibrain_methods.GetCurrentUnits
-            if builder then
-                if not self.Naval then
-                    if TAPrior.TechEnergyExist >= 75 and (bType == 'StructureForm' or TAPrior.GantryProduction >= 200 and bType == 'Other') then
-                        if bType == 'StructureForm' then
-                        if self.StructureForm > 3 then
-                        self.StructureForm = GetCurrentUnits(self.Brain, categories.STRUCTURE * (categories.CQUEMOV + categories.MASSFABRICATION))
-                        end
-                        if self.StructureForm < 2 then 
-                            return self:SCTAManagerLoopBody(builder, bType)
-                        end
-                    elseif self.Main and bType == 'Other' then
-                        if self.Other < 1 then
-                        self.Other = GetCurrentUnits(self.Brain, categories.EXPERIMENTAL * categories.MOBILE)
-                        end
-                        if self.Other > 0 then
-                            return self:SCTAManagerLoopBody(builder, bType)
-                        end
-                    end 
-                    --LOG('*TATerrain3', self.Main)
-                elseif bType == 'LandForm' then 
-                        if self.LandForm < 1 then
-                        self.LandForm = GetCurrentUnits(self.Brain, (categories.LAND * categories.MOBILE) - categories.ENGINEER - categories.SCOUT)
-                        end
-                        if self.LandForm > 0 then
-                            return self:SCTAManagerLoopBody(builder, bType)
-                        end    
-                elseif bType == 'AirForm' then 
-                        if self.AirForm < 1 then
-                        self.AirForm = GetCurrentUnits(self.Brain, (categories.AIR * categories.MOBILE) - categories.ENGINEER - categories.SCOUT)
-                        end
-                        if self.AirForm > 0 then
-                           return self:SCTAManagerLoopBody(builder, bType)
-                        end
-                elseif bType == 'Scout' then
-                    if self.Scout < 1 then
-                        self.Scout = GetCurrentUnits(self.Brain, (categories.armpw + categories.corgator + (categories.SCOUT + categories.AMPHIBIOUS) - categories.ENGINEER - categories.EXPERIMENTAL))
+        local aiBrain=self.Brain
+        if builder then
+            if aiBrain.Naval and bType == 'SeaForm' then 
+                
+                --LOG('*TATerrain', self.LocationType)
+                if aiBrain.SeaForm > 0 then
+                    return self:SCTAManagerLoopBody(builder, 'SeaForm')
+                end
+            end
+            if TAPrior.TechEnergyExist >= 75 and (bType == 'StructureForm' or TAPrior.GantryProduction >= 200 and bType == 'Other') then
+                if bType == 'StructureForm' then
+                
+                if aiBrain.StructureForm < 2 then 
+                    return self:SCTAManagerLoopBody(builder, bType)
+                end
+                elseif self.Main and bType == 'Other' then
+                    
+                    if aiBrain.Other > 0 then
+                        return self:SCTAManagerLoopBody(builder, bType)
                     end
-                        if self.Scout > 0 then
-                            if not self.Main then
-                                return self:SCTAManagerLoopBody(builder, bType)
-                            elseif self.Main and TAPrior.UnitProductionT1 >= 75 then 
-                                return self:SCTAManagerLoopBody(builder, bType)
-                            end
-                        end
+                end 
+            --LOG('*TATerrain3', self.Main)
+            elseif bType == 'LandForm' then 
+                
+                if aiBrain.LandForm > 0 then
+                    return self:SCTAManagerLoopBody(builder, bType)
                 end    
-                elseif self.Naval and bType == 'SeaForm' then 
-                    if self.SeaForm < 1 then
-                    self.SeaForm = GetCurrentUnits(self.Brain, (categories.NAVAL * categories.MOBILE) - categories.ENGINEER)
-                    end
-                    --LOG('*TATerrain', self.LocationType)
-                    if self.SeaForm > 0 then
-                       return self:SCTAManagerLoopBody(builder, 'SeaForm')
+            elseif bType == 'AirForm' then 
+                
+                if aiBrain.AirForm > 0 then
+                    return self:SCTAManagerLoopBody(builder, bType)
+                end
+            elseif bType == 'Scout' then
+                
+                if aiBrain.Scout > 0 then
+                    if not self.Main then
+                        return self:SCTAManagerLoopBody(builder, bType)
+                    elseif self.Main and TAPrior.UnitProductionT1 >= 75 then 
+                        return self:SCTAManagerLoopBody(builder, bType)
                     end
                 end
             end
-        end,
+        end
+    end,
         
     ----return self:ForkThread(self.SCTAManagerLoopBody
 
@@ -188,62 +179,3 @@ PlatoonFormManager = Class(SCTAPlatoonFormManager) {
     end,
 
 }
-
---[[            local GetUnitsAroundPoint = moho.aibrain_methods.GetUnitsAroundPoint
-        if builder then
-            if not self.Naval then
-                if TAPrior.TechEnergyExist >= 75 and (bType == 'StructureForm' or TAPrior.GantryProduction >= 200 and bType == 'Other') then
-                    if bType == 'StructureForm' then
-                    if self.StructureForm > 3 then
-                    self.StructureForm = GetUnitsAroundPoint(self.Brain, categories.STRUCTURE * (categories.CQUEMOV + categories.MASSFABRICATION), self.Location, self.Radius, 'Ally')
-                    end
-                    if self.StructureForm < 2 then 
-                        return self:SCTAManagerLoopBody(builder, bType)
-                    end
-                elseif self.Main and bType == 'Other' then
-                    if self.Other < 1 then
-                    self.Other = GetUnitsAroundPoint(self.Brain, categories.EXPERIMENTAL * categories.MOBILE, self.Location, self.Radius, 'Ally')
-                    end
-                    if self.Other > 0 then
-                        return self:SCTAManagerLoopBody(builder, bType)
-                    end
-                end 
-                --LOG('*TATerrain3', self.Main)
-            elseif bType == 'LandForm' then 
-                    if self.LandForm < 1 then
-                    self.LandForm = GetUnitsAroundPoint(self.Brain, (categories.LAND * categories.MOBILE) - categories.ENGINEER - categories.SCOUT, self.Location, self.Radius, 'Ally')
-                    end
-                    if self.LandForm > 0 then
-                        return self:SCTAManagerLoopBody(builder, bType)
-                    end    
-            elseif bType == 'AirForm' then 
-                    if self.AirForm < 1 then
-                    self.AirForm = GetUnitsAroundPoint(self.Brain, (categories.AIR * categories.MOBILE) - categories.ENGINEER - categories.SCOUT, self.Location, self.Radius, 'Ally')
-                    end
-                    if self.AirForm > 0 then
-                       return self:SCTAManagerLoopBody(builder, bType)
-                    end
-            elseif bType == 'Scout' then
-                if self.Scout < 1 then
-                    self.Scout = GetUnitsAroundPoint(self.Brain, categories.armpw + categories.corgator + ((categories.SCOUT + categories.AMPHIBIOUS) - categories.ENGINEER - categories.EXPERIMENTAL), self.Location, self.Radius, 'Ally')
-                end
-                    if self.Scout > 0 then
-                        if not self.Main then
-                            return self:SCTAManagerLoopBody(builder, bType)
-                        elseif self.Main and TAPrior.UnitProductionT1 >= 75 then 
-                            return self:SCTAManagerLoopBody(builder, bType)
-                        end
-                    end
-            end    
-            elseif self.Naval and bType == 'SeaForm' then 
-                if self.SeaForm < 1 then
-                self.SeaForm = GetUnitsAroundPoint(self.Brain, (categories.NAVAL * categories.MOBILE) - categories.ENGINEER, self.Location, self.Radius, 'Ally')
-                end
-                --LOG('*TATerrain', self.LocationType)
-                if self.SeaForm > 0 then
-                   return self:SCTAManagerLoopBody(builder, 'SeaForm')
-                end
-            end
-        end
-    end,
-    ]]
